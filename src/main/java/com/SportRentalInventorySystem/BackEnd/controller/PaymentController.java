cc896885080265c0d48cec22b11a391e590ce33b
Meron Seyoum <meryato@gmail.com>
2022-11-17 21:48:06 -0700

    port id

--- src/main/java/com/SportRentalInventorySystem/BackEnd/controller/PaymentController.java
+++ src/main/java/com/SportRentalInventorySystem/BackEnd/controller/PaymentController.java
@@ -7,7 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@CrossOrigin("https://sportsrentfrontend.herokuapp.com")
+@CrossOrigin(origins = "*" ) 
 @RequestMapping("/api/payment")
 public class PaymentController {
    
